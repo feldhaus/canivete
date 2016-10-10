033890f1c827e88d1b6df159c039030eaddca605
Maicon Feldhaus <maicon.feldhaus@gmail.com>
2016-10-10 16:31:32 -0300

    Added the function randomBetween

--- canivete.lua
+++ canivete.lua
@@ -26,7 +26,7 @@ local _M = {}
 
 -- shuffle a table
 function _M.shuffle( t )
-    assert( type( t ) == "table", "'shuffle' expected a table, got " .. type( t ) .. ")" )
+    assert( type( t ) == "table", "bad argument #1 to 'shuffle' (table expected, got " .. type( t ) .. ")" )
     
     local rand = math.random
     local iterations = #t
@@ -38,12 +38,22 @@ function _M.shuffle( t )
     end
 end
 
+-- returns a number between min and max values
+function _M.randomBetween( min, max )
+    -- bad argument #1 to 'function' (table expected, got string)
+    assert( type( min ) == "number", "bad argument #1 to 'randomBetween' (number expected, got " .. type( min ) .. ")" )
+    assert( type( max ) == "number", "bad argument #2 to 'randomBetween' (number expected, got " .. type( max ) .. ")" )
+    assert( min < max, "bad argument #1 to 'randomBetween' (min is greater than max)" )
+    
+    return min + math.random() * ( max - min )
+end
+
 -- sorts the table in ascending order
 function _M.orderAsc( t, key )
-    assert( type( t ) == "table", "'orderAsc' expected a table, got " .. type( t ) .. ")" )
+    assert( type( t ) == "table", "bad argument #1 to 'orderAsc' (table expected, got " .. type( t ) .. ")" )
     
     if key then
-        assert( type( key ) == "string", "'orderAsc' (expects a string, got " .. type( key ) .. ")" )
+        assert( type( key ) == "string", "bad argument #2 to 'orderAsc' (string expected, got " .. type( key ) .. ")" )
         table.sort( t, function( a,b ) return a[key] < b[key] end )
     else
         table.sort( t, function( a,b ) return a < b end )
@@ -52,10 +62,10 @@ end
 
 -- sort the table in a descending order
 function _M.orderDesc( t, key )
-    assert( type( t ) == "table", "'orderDsc' expected a table, got " .. type( t ) .. ")" )
+    assert( type( t ) == "table", "bad argument #1 to 'orderDsc' (table expected, got " .. type( t ) .. ")" )
     
     if key then
-        assert( type( key ) == "string", "'orderDsc' (expects a string, got " .. type( key ) .. ")" )
+        assert( type( key ) == "string", "bad argument #2 to 'orderDsc' (string expected, got " .. type( key ) .. ")" )
         table.sort( t, function(a,b) return a[key] > b[key] end )
     else
         table.sort( t, function(a,b) return a > b end )
@@ -77,7 +87,7 @@ end
 
 -- returns the sum table values
 function _M.sum( t )
-    assert( type( t ) == "table", "'sum' expected a table, got " .. type( t ) .. ")" )
+    assert( type( t ) == "table", "bad argument #1 to 'sum' (table expected, got " .. type( t ) .. ")" )
     
     local sum = 0
     for i=1, #t do sum = sum + t[i] end
@@ -86,32 +96,32 @@ end
 
 -- returns the average of table values
 function _M.avg( t )
-    assert( type( t ) == "table", "'avg' expected a table, got " .. type( t ) .. ")" )
+    assert( type( t ) == "table", "bad argument #1 to 'avg' (table expected, got " .. type( t ) .. ")" )
     
     return _M.sum( t ) / #t
 end
 
 -- clamps a value between a minimum number and maximum number value
 function _M.clamp( value, min, max )
-    assert( type( value ) == "number", "'clamp' expected a number, got " .. type( value ) .. ")" )
-    assert( type( min ) == "number", "'clamp' expected a number, got " .. type( min ) .. ")" )
-    assert( type( max ) == "number", "'clamp' expected a number, got " .. type( max ) .. ")" )
+    assert( type( value ) == "number", "bad argument #1 to 'clamp' (number expected, got " .. type( value ) .. ")" )
+    assert( type( min ) == "number", "bad argument #2 to 'clamp' (number expected, got " .. type( min ) .. ")" )
+    assert( type( max ) == "number", "bad argument #3 to 'clamp' (number expected, got " .. type( max ) .. ")" )
     
     return ( value < min ) and min or ( value > max ) and max or value
 end
 
 -- returns the interpolated number result between the two number values
 function _M.lerp( a, b, t )
-    assert( type( a ) == "number", "'lerp' expected a number, got " .. type( a ) .. ")" )
-    assert( type( b ) == "number", "'lerp' expected a number, got " .. type( b ) .. ")" )
-    assert( type( t ) == "number", "'lerp' expected a number, got " .. type( t ) .. ")" )
+    assert( type( a ) == "number", "bad argument #1 to 'lerp' (number expected, got " .. type( a ) .. ")" )
+    assert( type( b ) == "number", "bad argument #2 to 'lerp' (number expected, got " .. type( b ) .. ")" )
+    assert( type( t ) == "number", "bad argument #3 to 'lerp' (number expected, got " .. type( t ) .. ")" )
     
     return a + ( b - a ) * t
 end
 
 -- converts a hexadecimal string color to rgb (0-1)
 function _M.hex2rgb( hex )
-    assert( type( hex ) == "string", "'hex2rgb' expected a string, got " .. type( hex ) .. ")" )
+    assert( type( hex ) == "string", "bad argument #1 to 'hex2rgb' (string expected, got " .. type( hex ) .. ")" )
     
     local r = tonumber( "0x" .. hex:sub( 2, 3 ) ) / 255
     local g = tonumber( "0x" .. hex:sub( 4, 5 ) ) / 255
